c24d70f94f054e10e7e31c86b25f3d619a4e033a
comahqs <comahqs@163.com>
2019-09-29 15:44:58 +0800

    1、存储版本

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,17 @@
 ﻿#include <iostream>
-#include "service_net.h"
 #include <thread>
 #include <chrono>
-#include "service_context.h"
 #include "virsual_client.h"
-#include "server_media.h"
+#include "server_sip.h"
 
 
 int main(int , char** ){
     //GetACP();
-    server_sip s(5060);
-    s.start();
-    /*
-    service_context_ptr p_service_context = std::make_shared<service_context>();
+	system("chcp 65001");
+	auto psip = server_sip::get_instance();
+	psip->start(5060);
 
-    service_net service(5060);
-    service.start();
-
-    {
-        virsual_client client("127.0.0.1", 5060, p_service_context->get_context());
-        client.play();
-    }
-    */
-    //system("pause");
     std::this_thread::sleep_for(std::chrono::hours(1));
-    //service.stop();
+	psip->stop();
     return 0;
 }
